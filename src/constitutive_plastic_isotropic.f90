2dd293720b3e1ff6df12f01e5d3c2b2b8f809b13
Sharan Roongta <s.roongta@mpie.de>
2020-11-27 00:32:40 +0100

    math errors resolved

--- src/constitutive_plastic_isotropic.f90
+++ src/constitutive_plastic_isotropic.f90
@@ -38,7 +38,6 @@ submodule(constitutive:constitutive_plastic) plastic_isotropic
 
   type :: tIsotropicState
     real(pReal), pointer, dimension(:) :: &
-      xi, &
       xi_3d, &
       xi_2d, &
       gamma, &
@@ -148,7 +147,7 @@ module function plastic_isotropic_init() result(myPlasticity)
 !--------------------------------------------------------------------------------------------------
 ! allocate state arrays
     Nconstituents = count(material_phaseAt == p) * discretization_nIPs
-    sizeDotState = size(['xi_3d   ','gamma_3d','xi_2d   ','gamma_2d','xi      ','gamma   '])
+    sizeDotState = size(['xi_3d   ','gamma_3d','xi_2d   ','gamma_2d','gamma   '])
     sizeState = sizeDotState
 
     call constitutive_allocateState(plasticState(p),Nconstituents,sizeState,sizeDotState,0)
@@ -178,20 +177,13 @@ module function plastic_isotropic_init() result(myPlasticity)
     plasticState(p)%atol(4) = pl%get_asFloat('atol_gamma',defaultVal=1.0e-6_pReal)
     if (plasticState(p)%atol(4) < 0.0_pReal) extmsg = trim(extmsg)//' atol_gamma'
 
-    stt%xi  => plasticState(p)%state   (5,:)
-    stt%xi  =  xi_0 + xi_0_2d
-    dot%xi  => plasticState(p)%dotState(5,:)
-    plasticState(p)%atol(5) = pl%get_asFloat('atol_xi',defaultVal=1.0_pReal)
-    if (plasticState(p)%atol(5) < 0.0_pReal) extmsg = trim(extmsg)//' atol_xi'
-
-
-    stt%gamma  => plasticState(p)%state   (6,:)
-    dot%gamma  => plasticState(p)%dotState(6,:)
-    plasticState(p)%atol(6) = pl%get_asFloat('atol_gamma',defaultVal=1.0e-6_pReal)
-    if (plasticState(p)%atol(6) < 0.0_pReal) extmsg = trim(extmsg)//' atol_gamma'
+    stt%gamma  => plasticState(p)%state   (5,:)
+    dot%gamma  => plasticState(p)%dotState(5,:)
+    plasticState(p)%atol(5) = pl%get_asFloat('atol_gamma',defaultVal=1.0e-6_pReal)
+    if (plasticState(p)%atol(5) < 0.0_pReal) extmsg = trim(extmsg)//' atol_gamma'
 
     ! global alias
-    plasticState(p)%slipRate        => plasticState(p)%dotState(6:6,:)
+    plasticState(p)%slipRate        => plasticState(p)%dotState(5:5,:)
 
     plasticState(p)%state0 = plasticState(p)%state                                                  ! ToDo: this could be done centrally
 
@@ -226,14 +218,14 @@ module subroutine plastic_isotropic_LpAndItsTangent(Lp,dLp_dMp,Mp,h_n,instance,o
  
   real(pReal), dimension(3,3) :: &
     Mp_dev, &                                                                                       !< deviatoric part of the Mandel stress
-    proj_nrml                                                                  !normal projection of tensor on the plane
+    proj_nrml, &                                                                  !normal projection of tensor on the plane
+    Mp_nn_2d 
   real(pReal) :: &
     dot_gamma, &                                                                                    !< strainrate
     dot_gamma_3d, &
     dot_gamma_2d, &
     norm_Mp_dev, &                                                                                  !< norm of the deviatoric part of the Mandel stress
     squarenorm_Mp_dev, &                                                                            !< square of the norm of the deviatoric part of the Mandel stress
-    Mp_nn_2d, &
     Mp_ns_2d
   real(pReal), dimension(3) :: &
     h_s!, &                                                                         !direction vector of shear stress on the interface plane
@@ -248,13 +240,13 @@ module subroutine plastic_isotropic_LpAndItsTangent(Lp,dLp_dMp,Mp,h_n,instance,o
   norm_Mp_dev = sqrt(squarenorm_Mp_dev)
 
   proj_nrml = math_outer(h_n,h_n)
-  Mp_nn_2d= math_tensordot(Mp,proj_nrml)
-  Mp_ns_2d= norm2(matmul(Mp,h_n)-(Mp_nn_2d*h_n))
+  Mp_nn_2d= matmul(Mp,proj_nrml)
+  Mp_ns_2d= norm2(matmul(Mp,h_n)-matmul(Mp_nn_2d,h_n))
 
   if (norm_Mp_dev > 0.0_pReal) then
     dot_gamma_3d = prm%dot_gamma_0 * (sqrt(1.5_pReal) * norm_Mp_dev/(prm%M*stt%xi_3d(of))) **prm%n
 
-    h_s= (matmul(Mp,h_n)-(Mp_nn_2d*h_n)) / Mp_ns_2d
+    h_s= (matmul(Mp,h_n)-matmul(Mp_nn_2d,h_n)) / Mp_ns_2d
     dot_gamma_2d = prm%dot_gamma_0 * ( Mp_ns_2d/(prm%M_2d*stt%xi_2d(of))) **prm%n
     dot_gamma = dot_gamma_3d + dot_gamma_2d
     Lp = dot_gamma_3d/prm%M * Mp_dev/norm_Mp_dev + prm%phi * dot_gamma_2d/prm%M_2d * math_outer(h_s,h_n)
@@ -306,28 +298,8 @@ module subroutine plastic_isotropic_LiAndItsTangent(Li,dLi_dMi,Mi,instance,of)
 
   associate(prm => param(instance), stt => state(instance))
 
-  tr=math_trace33(math_spherical33(Mi))
-
-  if (prm%dilatation .and. abs(tr) > 0.0_pReal) then                                                 ! no stress or J2 plasticity --> Li and its derivative are zero
-    Li = math_I3 &
-       * prm%dot_gamma_0/prm%M * (3.0_pReal*prm%M*stt%xi(of))**(-prm%n) &
-       * tr * abs(tr)**(prm%n-1.0_pReal)
-
-#ifdef DEBUG
-    if (debugConstitutive%extensive .and. (of == prm%of_debug .or. .not. debugConstitutive%selective)) then
-      print'(/,a,/,f12.5)', '<< CONST isotropic >> pressure / MPa', tr/3.0_pReal*1.0e-6_pReal
-      print'(/,a,/,f12.5)', '<< CONST isotropic >> gdot', prm%dot_gamma_0 * (3.0_pReal*prm%M*stt%xi(of))**(-prm%n) &
-                                                           * tr * abs(tr)**(prm%n-1.0_pReal)
-    end if
-#endif
-
-    forall (k=1:3,l=1:3,m=1:3,n=1:3) &
-      dLi_dMi(k,l,m,n) = prm%n / tr * Li(k,l) * math_I3(m,n)
-
-  else
     Li      = 0.0_pReal
     dLi_dMi = 0.0_pReal
-  endif
 
   end associate
 
@@ -353,18 +325,18 @@ module subroutine plastic_isotropic_dotState(Mp,h_n,instance,of)
     norm_Mp                                                                                         !< norm of the (deviatoric) Mandel stress
 
   real(pReal), dimension(3,3) :: &
-    proj_nrml                                                                                          !<normal projection tensor on the plane(vahid)
+    proj_nrml, &                                                                                          !<normal projection tensor on the plane(vahid)
+    Mp_nn_2d                                                                                     !< normal stress on the plane(vahid)
   real(pReal) :: &
     dot_gamma_3d, &
     dot_gamma_2d, &                                                                                 !< strainrate in 2d system(vahid)
-    Mp_nn_2d, &                                                                                     !< normal stress on the plane(vahid)
     Mp_ns_2d                                                                                        !< shear stress on the plane(vahid)
 
   associate(prm => param(instance), stt => state(instance), dot => dotState(instance))
 
   proj_nrml = math_outer(h_n,h_n)
-  Mp_nn_2d= math_tensordot(Mp,proj_nrml)
-  Mp_ns_2d= norm2(matmul(Mp,h_n)-(Mp_nn_2d*h_n))
+  Mp_nn_2d= matmul(Mp,proj_nrml)
+  Mp_ns_2d= norm2(matmul(Mp,h_n)-matmul(Mp_nn_2d,h_n))
 
 
   if (prm%dilatation) then
@@ -394,11 +366,9 @@ module subroutine plastic_isotropic_dotState(Mp,h_n,instance,of)
                *  prm%h_0_2d  &
                * abs( 1.0_pReal - stt%xi_2d(of)/prm%xi_inf_2d )**prm%a &
                * sign(1.0_pReal, 1.0_pReal - stt%xi_2d(of)/prm%xi_inf_2d)
-    dot%xi(of) = dot%xi_3d(of) + dot%xi_2d(of)
   else
     dot%xi_3d(of) = 0.0_pReal
     dot%xi_2d(of) = 0.0_pReal
-    dot%xi(of) = dot%xi_3d(of) + dot%xi_2d(of)
   endif
 
   dot%gamma_3d(of) = dot_gamma_3d                                                                         ! ToDo: not really used
@@ -423,23 +393,20 @@ module subroutine plastic_isotropic_results(instance,group)
   associate(prm => param(instance), stt => state(instance))
   outputsLoop: do o = 1,size(prm%output)
     select case(trim(prm%output(o)))
-      case ('xi')
-        call results_writeDataset(group,stt%xi,trim(prm%output(o)), &
-                                    'resistance against plastic flow','Pa')
       case ('xi_3d')
-        call results_writeDataset(group,stt%xi,trim(prm%output(o)), &
+        call results_writeDataset(group,stt%xi_3d,trim(prm%output(o)), &
                                     'resistance against plastic flow for 3d','Pa')
       case ('xi_2d')
-        call results_writeDataset(group,stt%xi,trim(prm%output(o)), &
+        call results_writeDataset(group,stt%xi_2d,trim(prm%output(o)), &
                                    'resistance against plastic flow for 2d','Pa')
       case ('gamma')
-        call results_writeDataset(group,stt%xi,trim(prm%output(o)), &
+        call results_writeDataset(group,stt%gamma,trim(prm%output(o)), &
                                    'total plastic shear','1')
       case ('gamma_3d')
-        call results_writeDataset(group,stt%xi,trim(prm%output(o)), &
+        call results_writeDataset(group,stt%gamma_3d,trim(prm%output(o)), &
                                    'plastic shear for 3d','1')
       case ('gamma_2d')
-        call results_writeDataset(group,stt%xi,trim(prm%output(o)), &
+        call results_writeDataset(group,stt%gamma_2d,trim(prm%output(o)), &
                                   'plastic shear for 2d','1')
  
     end select
